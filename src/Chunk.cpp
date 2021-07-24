4251dcea25086b0c6dee65cd1d5eba37eaad4130
Hendrik Ziegler <hendrikziegler@outlook.de>
2021-07-24 15:32:32 +0200

    Added constants to Chunk + methods to write to the chunk

--- src/Chunk.cpp
+++ src/Chunk.cpp
@@ -1,1 +1,13 @@
 #include "Chunk.h"
+
+void Chunk::writeChunk(uint8_t byte, int line)
+{
+    code.push_back(byte);
+    lines.push_back(line);
+}
+
+int Chunk::addConstant(Value val)
+{
+    constants.push_back(val);
+    return constants.size() - 1;
+}
--- src/Chunk.h
+++ src/Chunk.h
@@ -1,17 +1,25 @@
 #pragma once
 
 #include <vector>
+#include "Value.h"
 
 //enum class for all operation codes that the VirtualMachine will process
 enum class OpCode
 {
-	OP_RETURN
+	CONSTANT,
+	RETURN
 };
 
 //holds byte-code and constant Values
 class Chunk
 {
 public:
+	void writeChunk(uint8_t byte, int line);
+	int addConstant(Value val);
+
+public:
 	std::vector<uint8_t> code; //the byte code
+	std::vector<Value> constants; //constant values
+	std::vector<int> lines; //maps the code array. each byte in code has a line at the same index here
 };
 
