7ea4bb4d473b75e10a3482f9a852e01e2fb1f29e
Hendrik Ziegler <hendrikziegler@outlook.de>
2021-07-25 11:58:09 +0200

    added basics of the Scanner class

---
+++ src/Scanner.cpp
@@ -0,0 +1,47 @@
+#include "Scanner.h"
+#include <fstream>
+#include <streambuf>
+
+Scanner::Scanner(const std::string& file)
+	:
+	file(file),
+	line(1),
+	currentDepth(0)
+{
+	std::ifstream ifs;
+	ifs.open(file);
+	if (!ifs.is_open())
+		throw file_exception(GENERATE_EXCEPTION(file_exception, "Could not open '" + file + "'"));
+	source = std::string((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+}
+
+Token Scanner::scanToken()
+{
+	skipWhitespaces();
+	return Token();
+}
+
+bool Scanner::isAtEnd() const
+{
+	return *current == '\0';
+}
+
+Token Scanner::makeToken(TokenType type) const
+{
+	Token token;
+	token.depth = currentDepth;
+	token.type = type;
+	token.line = line;
+	token.literal = std::string(current, start);
+	return Token();
+}
+
+Token Scanner::errorToken(std::string msg) const
+{
+	return Token();
+}
+
+void Scanner::skipWhitespaces()
+{
+
+}
---
+++ src/Scanner.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include "common.h"
+
+class file_exception : public base_exception
+{
+public:
+	file_exception(const std::string& msg) : base_exception(msg) {}
+};
+
+enum class TokenType
+{
+	//syntax
+	COLO, DOT, COMMA, LEFT_PAREN, RIGHT_PAREN,
+	//literals
+	IDENTIFIER, STRING, CHARACTER, INUMBER, DNUMBER,
+	//maths
+	NEGATEMINUS,
+	PLUS, MINUS, MAL, DURCH, MODULO, HOCH, WURZEL, LN, BETRAG,
+	PI, E, TAU,
+	//boolsche operatoren
+	UND, ODER, NICHT,
+	KLEINER, GROESSER, ALS, KLEINERODER, GROESSERODER, UNGLEICH, GLEICH,
+	//bitshifting
+	UM, BIT, NACH, RECHTS, LINKS, VERSCHOBEN,
+	//bitwise
+	LOGISCH, KONTRA, LOGISCHNICHT,
+	//if
+	WENN, ABER, DANN, SONST,
+	//for
+	FUER, JEDE, VON, BIS, SCHRITTGROESSE,
+	//while
+	SOLANGE,
+	//loops
+	MACHE,
+	//functions
+	FUNKTION, NIMMT, MIT, MACHT, VOM, TYP,
+	//assignement
+	IST, SIND,
+	//Artikel
+	DER, DIE, DAS,
+	//Typen
+	ZAHL, KOMMAZAHL, BOOLEAN, ZEICHENKETTE, ZEICHEN,
+	//Arrays
+	ZAHLEN, KOMMAZAHLEN, ZEICHENKETTEN, AN, STELLE,
+	//bool literals
+	WAHR, FALSCH,
+	//return
+	GIB, ZURUECK,
+#ifdef _MDEBUG_
+	PRINT,
+#endif
+	DIRECTIVE,
+	ERROR, END
+};
+
+struct Token
+{
+	TokenType type;
+	std::string literal;
+	int line;
+	int depth;
+};
+
+class Scanner
+{
+public:
+	Scanner(const std::string& file);
+
+	Token scanToken();
+
+private:
+	/**Functions used in scanToken**/
+
+	bool isAtEnd() const;
+	Token makeToken(TokenType type) const;
+	Token errorToken(std::string msg) const;
+	void skipWhitespaces();
+private:
+	const std::string file; //path to the source file
+	std::string source; //the source code
+
+	std::string::iterator start; //start of the current token
+	std::string::iterator current; //one after the current character
+	int line; //current line in the source code
+	int currentDepth; //depth on the current line
+};
+
