775670f48afcdb33640e516c5d63dded5bce04bf
Hendrik Ziegler <hendrikziegler@outlook.de>
2021-08-11 14:44:55 +0200

    Added boilerplate code for expression parsing

--- src/Compiler.cpp
+++ src/Compiler.cpp
@@ -1,4 +1,5 @@
 #include "Compiler.h"
+#include <iostream>
 
 Compiler::Compiler(const std::string& filePath,
 	std::unordered_map<std::string, Value>* globals,
@@ -8,6 +9,7 @@ Compiler::Compiler(const std::string& filePath,
 	globals(globals),
 	functions(functions),
 	hadError(false),
+	panicMode(false),
 	currentFunction(nullptr)
 {}
 
@@ -35,9 +37,163 @@ uint8_t Compiler::makeConstant(Value value)
 	//lastEmittedType = value.getType();
 	int constant = currentChunk()->addConstant(std::move(value));
 	if (constant > UINT8_MAX) {
-		//error(u8"Zu viele Konstanten in diesem Chunk!");
-		return 0;
+		error(u8"Zu viele Konstanten in diesem Chunk!");
+		return -1;
 	}
 
 	return constant;
+}
+
+void Compiler::error(std::string msg, std::vector<Token>::iterator where)
+{
+	if (panicMode) return;
+	panicMode = true;
+	std::cerr << u8"[Zeile " << where->line << u8"] Fehler ";
+
+	if (where->type == TokenType::END) std::cerr << u8"am Ende";
+	else if (where->type == TokenType::ERROR);
+	else std::cerr << u8"bei '" << where->literal << u8"'";
+
+	std::cerr << u8": " << msg << "\n";
+	hadError = true;
+}
+
+void Compiler::error(std::string msg)
+{
+	error(msg, preIt);
+}
+
+void Compiler::advance()
+{
+	preIt = currIt;
+	while (true)
+	{
+		currIt++;
+		if (currIt == tokens.end()) currIt--;
+		if (currIt->type != TokenType::ERROR) break;
+
+		error(currIt->literal, currIt);
+	}
+}
+
+void Compiler::consume(TokenType type, std::string errorMsg)
+{
+	if (currIt->type == type)
+	{
+		advance();
+		return;
+	}
+
+	error(errorMsg, currIt);
+}
+
+bool Compiler::match(TokenType type)
+{
+	if (currIt->type != type) return false;
+	advance();
+	return true;
+}
+
+bool Compiler::check(TokenType type)
+{
+	return currIt->type == type;
+}
+
+bool Compiler::checkNext(TokenType type)
+{
+	return ((currIt + 1) != tokens.end()) && ((currIt + 1)->type == type);
+}
+
+ValueType Compiler::expression()
+{
+	return parsePrecedence(Precedence::Assignement);
+}
+
+ValueType Compiler::parsePrecedence(Precedence precedence)
+{
+	return ValueType();
+}
+
+ValueType Compiler::dnumber(bool canAssign)
+{
+	error("Not implemented yet!");
+	return ValueType::None;
+}
+
+ValueType Compiler::inumber(bool canAssign)
+{
+	error("Not implemented yet!");
+	return ValueType::None;
+}
+
+ValueType Compiler::string(bool canAssign)
+{
+	error("Not implemented yet!");
+	return ValueType::None;
+}
+
+ValueType Compiler::character(bool canAssign)
+{
+	error("Not implemented yet!");
+	return ValueType::None;
+}
+
+ValueType Compiler::arrLiteral(bool canAssign)
+{
+	error("Not implemented yet!");
+	return ValueType::None;
+}
+
+ValueType Compiler::Literal(bool canAssign)
+{
+	error("Not implemented yet!");
+	return ValueType::None;
+}
+
+ValueType Compiler::grouping(bool canAssign)
+{
+	error("Not implemented yet!");
+	return ValueType::None;
+}
+
+ValueType Compiler::unary(bool canAssign)
+{
+	error("Not implemented yet!");
+	return ValueType::None;
+}
+
+ValueType Compiler::binary(bool canAssign)
+{
+	error("Not implemented yet!");
+	return ValueType::None;
+}
+
+ValueType Compiler::bitwise(bool canAssign)
+{
+	error("Not implemented yet!");
+	return ValueType::None;
+}
+
+ValueType Compiler::and_(bool canAssign)
+{
+	error("Not implemented yet!");
+	return ValueType::None;
+}
+
+ValueType Compiler::or_(bool canAssign)
+{
+	error("Not implemented yet!");
+	return ValueType::None;
+}
+
+ValueType Compiler::variable(bool canAssign)
+{
+	error("Not implemented yet!");
+	return ValueType::None;
+}
+
+ValueType Compiler::index(bool canAssign)
+{
+	error("Not implemented yet!");
+	return ValueType::None;
 }
--- src/Compiler.h
+++ src/Compiler.h
@@ -5,6 +5,8 @@
 
 class Compiler
 {
+private:
+	using op = OpCode;
 public:
 	Compiler(const std::string& filePath, std::unordered_map<std::string, Value>* globals, std::unordered_map<std::string, Function>* functions);
 
@@ -20,18 +22,166 @@ private:
 	void emitReturn() { emitByte(OpCode::RETURN); };
 	void emitConstant(Value value) { emitBytes(OpCode::CONSTANT, makeConstant(std::move(value))); };
 
-	uint8_t makeConstant(Value value);
+	uint8_t makeConstant(Value value); //add a constant to the current chunk and return its index
+
+	void error(std::string msg, std::vector<Token>::iterator where);
+	void error(std::string msg);
+
+	//Functions to work with the tokens
+
+	void advance();
+	void consume(TokenType type, std::string errorMsg); //if currIt matches the type we advance, else we report an error with errorMsg
+	bool match(TokenType type); //if currIt matches the type we advance and return true, else we return false and don't advance
+	bool check(TokenType type); //same as match but without advancing
+	bool checkNext(TokenType type); //check but the Token after currIt
+private:
+	enum class Precedence
+	{
+		None,
+		Assignement, // =
+		Or, // ||
+		And, // &&
+		Bitwise, // ^ ~ & |
+		Equality, // == !=
+		Comparison, // < > <= >=
+		Bitshift, // << >>
+		Term, // + -
+		Factor, // * / %
+		Exponent, // pow sqrt
+		Unary, // ! -
+		Indexing, // []
+		Call, // ()
+		Primary
+	};
+private:
+	ValueType expression(); //parse the next expression and return the type it evaluates to
+	ValueType parsePrecedence(Precedence precedence); //parse the next operator/expression with the given precedence
+
+	//parse functions for all operators
+
+	ValueType dnumber(bool canAssign);
+	ValueType inumber(bool canAssign);
+	ValueType string(bool canAssign);
+	ValueType character(bool canAssign);
+	ValueType arrLiteral(bool canAssign);
+	ValueType Literal(bool canAssign);
+	ValueType grouping(bool canAssign);
+	ValueType unary(bool canAssign);
+	ValueType binary(bool canAssign);
+	ValueType bitwise(bool canAssign);
+	ValueType and_(bool canAssign);
+	ValueType or_(bool canAssign);
+	ValueType variable(bool canAssign);
+	ValueType index(bool canAssign);
+private:
+	using MemFunctPtr = ValueType(Compiler::*)(bool); //pointer to a member function of Compiler that takes a bool and returns a ValueType
+	struct ParseRule
+	{
+		MemFunctPtr prefix; //function for the prefix use of the operator
+		MemFunctPtr infix; //function for the infix use of the operator
+		Precedence precedence; //the precedende of the operator
+	};
+	static inline const std::unordered_map<TokenType, ParseRule> parseRules = {
+		{ TokenType::LEFT_PAREN,	ParseRule{&Compiler::grouping, nullptr,			Precedence::Call}},
+		{ TokenType::RIGHT_PAREN,	ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::COLON,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::COMMA,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::DOT,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::MINUS,			ParseRule{nullptr,			&Compiler::binary,  Precedence::Term}},
+		{ TokenType::NEGATEMINUS,   ParseRule{&Compiler::unary,	nullptr,			Precedence::Term}},
+		{ TokenType::PLUS,			ParseRule{nullptr,			&Compiler::binary,  Precedence::Term}},
+		{ TokenType::DURCH,			ParseRule{nullptr,			&Compiler::binary,  Precedence::Factor}},
+		{ TokenType::MAL,			ParseRule{nullptr,			&Compiler::binary,  Precedence::Factor}},
+		{ TokenType::NICHT,			ParseRule{&Compiler::unary,	nullptr,			Precedence::Unary}},
+		{ TokenType::MODULO,		ParseRule{nullptr,			&Compiler::binary,	Precedence::Term}},
+		{ TokenType::HOCH,			ParseRule{nullptr,			&Compiler::binary,	Precedence::Exponent}},
+		{ TokenType::WURZEL,		ParseRule{nullptr,			&Compiler::binary,  Precedence::Exponent}},
+		{ TokenType::UM,			ParseRule{nullptr,			&Compiler::binary,	Precedence::Bitshift}},
+		{ TokenType::LOGISCH,		ParseRule{&Compiler::bitwise,nullptr,			Precedence::Bitwise}},
+		{ TokenType::LOGISCHNICHT,  ParseRule{&Compiler::unary, nullptr,			Precedence::Unary}},
+		{ TokenType::LN,			ParseRule{&Compiler::unary, nullptr,			Precedence::Unary}},
+		{ TokenType::BETRAG,		ParseRule{&Compiler::unary, nullptr,			Precedence::Unary}},
+		{ TokenType::UNGLEICH,		ParseRule{nullptr,			&Compiler::binary,	Precedence::Equality}},
+		{ TokenType::GLEICH,		ParseRule{nullptr,			&Compiler::binary,	Precedence::Equality}},
+		{ TokenType::GROESSER,		ParseRule{nullptr,			&Compiler::binary,	Precedence::Comparison}},
+		{ TokenType::KLEINER,		ParseRule{nullptr,			&Compiler::binary,	Precedence::Comparison}},
+		{ TokenType::GROESSERODER,	ParseRule{nullptr,			&Compiler::binary,	Precedence::Comparison}},
+		{ TokenType::KLEINERODER,	ParseRule{nullptr,			&Compiler::binary,	Precedence::Comparison}},
+		{ TokenType::IDENTIFIER,	ParseRule{&Compiler::variable,nullptr,			Precedence::None}},
+		{ TokenType::STRING,		ParseRule{&Compiler::string,nullptr,			Precedence::None}},
+		{ TokenType::INUMBER,		ParseRule{&Compiler::inumber,nullptr,			Precedence::None}},
+		{ TokenType::DNUMBER,		ParseRule{&Compiler::dnumber,nullptr,			Precedence::None}},
+		{ TokenType::CHARACTER,		ParseRule{&Compiler::character,nullptr,			Precedence::None}},
+		{ TokenType::UND,			ParseRule{nullptr,			&Compiler::and_,	Precedence::And}},
+		{ TokenType::SONST,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::FALSCH,		ParseRule{&Compiler::Literal,nullptr,			Precedence::None}},
+		{ TokenType::PI,			ParseRule{&Compiler::Literal,nullptr,			Precedence::None}},
+		{ TokenType::TAU,			ParseRule{&Compiler::Literal,nullptr,			Precedence::None}},
+		{ TokenType::E,				ParseRule{&Compiler::Literal,nullptr,			Precedence::None}},
+		{ TokenType::PHI,			ParseRule{&Compiler::Literal,nullptr,			Precedence::None}},
+		{ TokenType::FUER,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::FUNKTION,		ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::WENN,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::ODER,			ParseRule{nullptr,			&Compiler::or_,		Precedence::Or}},
+		{ TokenType::IST,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::GIB,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::ZURUECK,		ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::WAHR,			ParseRule{&Compiler::Literal,nullptr,			Precedence::None}},
+		{ TokenType::ZAHL,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::KOMMAZAHL,		ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::BOOLEAN,		ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::ZEICHEN,		ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::ZEICHENKETTE,	ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::ZAHLEN,		ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::BOOLEANS,		ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::KOMMAZAHLEN,	ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::ZEICHENKETTEN,	ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::AN,			ParseRule{nullptr,			&Compiler::index,	Precedence::Call}},
+		{ TokenType::STELLE,		ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::STUECK,		ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::LEFT_SQAREBRACKET,ParseRule{&Compiler::arrLiteral,	nullptr,	Precedence::None}},
+		{ TokenType::RIGHT_SQAREBRACKET,ParseRule{nullptr,		nullptr,			Precedence::None}},
+		{ TokenType::SEMICOLON,		ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::SOLANGE,		ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::ALS,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::BIT,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::NACH,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::RECHTS,		ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::LINKS,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::VERSCHOBEN,	ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::KONTRA,		ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::ABER,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::DANN,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::JEDE,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::VON,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::BIS,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::MIT,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::SCHRITTGROESSE,ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::MACHE,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::VOM,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::TYP,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::SIND,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::DER,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::DIE,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::DAS,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::ERROR,			ParseRule{nullptr,			nullptr,			Precedence::None}},
+		{ TokenType::END,			ParseRule{nullptr,			nullptr,			Precedence::None}}
+#ifdef _MDEBUG_
+		,{TokenType::PRINT,			ParseRule{nullptr,			nullptr,			Precedence::None}}
+#endif
+	};
 private:
 	const std::string filePath;
 
 	std::unordered_map<std::string, Value>* globals;
 	std::unordered_map<std::string, Function>* functions;
 
-	bool hadError;
+	bool hadError; //did an error occure?
+	bool panicMode; //are we currently handling an error?
 private:
-	std::vector<Token> tokens;
+	std::vector<Token> tokens; //output from the scanner
 	std::vector<Token>::iterator preIt; //previously scanned token
-	std::vector<Token>::iterator curIt; //current token
+	std::vector<Token>::iterator currIt; //current token
 
 	Function* currentFunction; //the function that is currently being compiled (most often the nameless main function)
 };
